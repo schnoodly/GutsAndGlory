4de09b3ad1f610e3f14bb61d6a3f92583031bde9
Mackenzie Fritschle <fritschlem@gmail.com>
2020-04-19 10:03:48 -0500

    Initial commit

---
+++ GutsAndGlory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace GutsAndGlory
+{
+    public class Initialize
+    {
+        MBSubModuleBase mbsmb = new MBSubModuleBase();
+            OnSubModuleLoad()
+        {
+             InformationManager.DisplayMessage(new InformationMessage("Guts And Glory Loaded"));
+        }
+
+        OnKill()
+        {
+            if(Damage > 35f)
+            {
+                RemoveMeshesWithTag(this.tag)
+            }
+        }
+    }
+}
